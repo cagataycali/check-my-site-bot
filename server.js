fb0fa9d4086f9ad9abcdeb11f654e2f03225eda6
cagataycali <cagataycali@icloud.com>
2017-04-04 15:38:02 +0300

    headers

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 /* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }]*/
 /* eslint no-lonely-if: "error"*/
-console.log(process.env);
 const TelegramBot = require('node-telegram-bot-api');
 const CronJob = require('cron').CronJob;
 const ran = require('./ran');
